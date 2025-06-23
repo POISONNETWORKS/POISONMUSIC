bab34c62e175f2256758ff77c8c0f311a37c7cb2
- ᴘᴏɪsᴏɴ ηєᴛᴡσᴙᴋ /³ <mahtoxanjali@gmail.com>
2025-06-23 11:34:52 +0530

    - ᴘᴏɪsᴏɴ ηєᴛᴡσᴙᴋ </³

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # ───── Git & Updates ───── #
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/POISONNETWORKS/POISONMUSIC")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN")
 
 # ───── Support & Community ───── #
