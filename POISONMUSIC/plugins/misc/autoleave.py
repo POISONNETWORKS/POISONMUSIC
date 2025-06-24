dbecf8eab98f4739ffee2ef46b2c1202be5b482a
- ᴘᴏɪsᴏɴ ηєᴛᴡσᴙᴋ /³ <mahtoxanjali@gmail.com>
2025-06-24 12:41:12 +0530

    - ᴘᴏɪsᴏɴ ηєᴛᴡσᴙᴋ </³

--- POISONMUSIC/plugins/misc/autoleave.py
+++ POISONMUSIC/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1002601268013
-                                and i.chat.id != -1002601268013
+                                and i.chat.id != -1002486089758
+                                
                                 
                             ):
                                 if left == 20:
