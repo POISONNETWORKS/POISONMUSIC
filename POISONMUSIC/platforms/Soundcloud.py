222474371ab306b9178a229d28ba0e999863d088
- ᴘᴏɪsᴏɴ ηєᴛᴡσᴙᴋ /³ <mahtoxanjali@gmail.com>
2025-06-25 17:30:28 +0530

    - ᴘᴏɪsᴏɴ ηєᴛᴡσᴙᴋ </³

--- POISONMUSIC/platforms/Soundcloud.py
+++ POISONMUSIC/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from POISONMUSIC.utils.formatters import seconds_to_min
+from PoisonMUSIC.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
