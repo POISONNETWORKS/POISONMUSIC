c6f679be069b618cf44351079db5ab9efd41cfd0
- ᴘᴏɪsᴏɴ ηєᴛᴡσᴙᴋ /³ <mahtoxanjali@gmail.com>
2025-06-26 11:23:21 +0530

    - ᴘᴏɪsᴏɴ ηєᴛᴡσᴙᴋ </³

--- POISONMUSIC/plugins/bot/inline.py
+++ POISONMUSIC/plugins/bot/inline.py
@@ -36,22 +36,22 @@ async def inline_query_handler(client, query):
                 [
                     [
                         InlineKeyboardButton(
-                            text="ʏᴏᴜᴛᴜʙᴇ 🎄",
+                            text="• ʏσυᴛυʙє •",
                             url=link,
                         )
                     ],
                 ]
             )
             searched_text = f"""
-❄ <b>ᴛɪᴛʟᴇ :</b> <a href={link}>{title}</a>
+❄ <b>ᴛɪᴛʟє :</b> <a href={link}>{title}</a>
 
-⏳ <b>ᴅᴜʀᴀᴛɪᴏɴ :</b> {duration} ᴍɪɴᴜᴛᴇs
-👀 <b>ᴠɪᴇᴡs :</b> <code>{views}</code>
-🎥 <b>ᴄʜᴀɴɴᴇʟ :</b> <a href={channellink}>{channel}</a>
-⏰ <b>ᴘᴜʙʟɪsʜᴇᴅ ᴏɴ :</b> {published}
+⏳ <b>ᴅυʀᴧᴛɪση :</b> {duration} ϻɪηυᴛєs
+👀 <b>ᴠɪєᴡs :</b> <code>{views}</code>
+🎥 <b>ᴄʜᴧηηєʟ :</b> <a href={channellink}>{channel}</a>
+⏰ <b>ᴘυʙʟɪsʜєᴅ ση :</b> {published}
 
 
-<u><b>➻ ɪɴʟɪɴᴇ sᴇᴀʀᴄʜ ᴍᴏᴅᴇ ʙʏ {app.name}</b></u>"""
+<u><b>➻ ɪηʟɪηє sєᴧʀᴄʜ ᴍσᴇє ʙʏ {app.name}</b></u>"""
             answers.append(
                 InlineQueryResultPhoto(
                     photo_url=thumbnail,
