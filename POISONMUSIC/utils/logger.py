043508e4bb911d3d7705df9ff0047980676b910d
- ᴘᴏɪsᴏɴ ηєᴛᴡσᴙᴋ /³ <mahtoxanjali@gmail.com>
2025-07-02 10:58:21 +0530

    - ᴘᴏɪsᴏɴ ηєᴛᴡσᴙᴋ </³

--- POISONMUSIC/utils/logger.py
+++ POISONMUSIC/utils/logger.py
@@ -8,18 +8,18 @@ from config import LOGGER_ID
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+<b>{app.mention} ᴘʟᴧʏ ʟσɢ</b>
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
+<b>ᴄʜᴧᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
+<b>ᴄʜᴧᴛ ηᴧϻє :</b> {message.chat.title}
+<b>ᴄʜᴧᴛ ᴜsєʀηᴧϻє :</b> @{message.chat.username}
 
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+<b>ᴄʜᴧᴛ ɪᴅ :</b> <code>{message.from_user.id}</code>
+<b>ηᴧϻє :</b> {message.from_user.mention}
+<b>ᴜsєʀηᴧϻє :</b> @{message.from_user.username}
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+<b>ǫᴜєʀʏ :</b> {message.text.split(None, 1)[1]}
+<b>sᴛʀєᴧϻᴛʏᴘє :</b> {streamtype}"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
