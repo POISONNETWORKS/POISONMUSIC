1bc542446d1548ed4d4add66ad6c51a9667f3b62
- ᴘᴏɪsᴏɴ ηєᴛᴡσᴙᴋ /³ <mahtoxanjali@gmail.com>
2025-07-06 20:11:01 +0530

    - ᴘᴏɪsᴏɴ ηєᴛᴡσᴙᴋ </³

--- POISONMUSIC/platforms/Telegram.py
+++ POISONMUSIC/platforms/Telegram.py
@@ -37,9 +37,9 @@ class TeleAPI:
         try:
             file_name = file.file_name
             if file_name is None:
-                file_name = "ᴛᴇʟᴇɢʀᴀᴍ ᴀᴜᴅɪᴏ" if audio else "ᴛᴇʟᴇɢʀᴀᴍ ᴠɪᴅᴇᴏ"
+                file_name = "ᴛєʟєɢʀᴧϻ ᴧυᴅɪσ" if audio else "ᴛєʟєɢʀᴧϻ ᴠɪᴅєσ"
         except:
-            file_name = "ᴛᴇʟᴇɢʀᴀᴍ ᴀᴜᴅɪᴏ" if audio else "ᴛᴇʟᴇɢʀᴀᴍ ᴠɪᴅᴇᴏ"
+            file_name = "ᴛєʟєɢʀᴧϻ ᴧυᴅɪσ" if audio else "ᴛєʟєɢʀᴧϻ ᴠɪᴅєσ"
         return file_name
 
     async def get_duration(self, file):
@@ -110,7 +110,7 @@ class TeleAPI:
                     [
                         [
                             InlineKeyboardButton(
-                                text="ᴄᴀɴᴄᴇʟ",
+                                text="ᴄᴧηᴄєʟ",
                                 callback_data="stop_downloading",
                             ),
                         ]
@@ -122,7 +122,7 @@ class TeleAPI:
                 eta = int((total - current) / speed)
                 eta = get_readable_time(eta)
                 if not eta:
-                    eta = "0 sᴇᴄᴏɴᴅs"
+                    eta = "0 sєᴄσηᴅs"
                 total_size = convert_bytes(total)
                 completed_size = convert_bytes(current)
                 speed = convert_bytes(speed)
@@ -161,7 +161,7 @@ class TeleAPI:
                         int(int(time.time()) - int(speed_counter[message.id]))
                     )
                 except:
-                    elapsed = "0 sᴇᴄᴏɴᴅs"
+                    elapsed = "0 sєᴄσηᴅs"
                 await mystic.edit_text(_["tg_2"].format(elapsed))
             except:
                 await mystic.edit_text(_["tg_3"])
